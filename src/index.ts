2eac799c045763890c9795935a5b6b79220b8883
royantar0311 <royantar1@gmail.com>
2021-02-13 13:53:20 +0600

    pg added & nodemon working

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
 import * as bodyparser from "body-parser";
 import cors from "cors";
 import debug from "debug";
+import * as dotenv from "dotenv";
 import express from "express";
 import * as expressWinston from "express-winston";
 import * as http from "http";
+import { Client as pgClient } from "pg";
 import * as winston from "winston";
-import { CommonRoutesConfig } from "./common/common.routes.config";
-import { UsersRoutes } from "./users/users.routes.config";
+import { CommonRoutesConfig } from "../common/common.routes.config";
+import { UsersRoutes } from "../users/users.routes.config";
+dotenv.config();
 
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
 const port: Number = 3000;
 const routes: Array<CommonRoutesConfig> = [];
 const debugLog: debug.IDebugger = debug("index");
+const debugLogDB: debug.IDebugger = debug("index-db");
 app.use(bodyparser.json());
 app.use(cors());
 app.use(
@@ -34,6 +38,23 @@ app.use(
     ),
   })
 );
+(async () => {
+  const client = new pgClient({
+    host: process.env.PG_HOST,
+    user: process.env.PG_USERNAME,
+    password: process.env.PG_PASSWORD,
+    port: Number.parseInt(process.env.PG_PORT),
+    database: process.env.PG_DATABASE,
+  });
+  try {
+    await client.connect();
+    const query = await client.query(`SELECT * FROM users LIMIT 1`);
+    debugLogDB("db connected " + query.rows[0].name);
+    //c
+  } catch (err) {
+    debugLogDB(err);
+  }
+})();
 app.get("/", (req: express.Request, res: express.Response) => {
   res.status(200).send(`Server up and running!`);
 });
